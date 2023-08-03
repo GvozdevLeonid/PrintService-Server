2c01ec866ffbd37976fb56a3f27bafbd32b86ff2
GvozdevLeonid <gvozdev-leonid-95@yandex.ru>
2023-08-04 00:08:58 +0200

    add comand for creating guest account

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,7 @@ python manage.py tailwind install --no-input
 python manage.py tailwind build --no-input
 echo yes | python manage.py collectstatic
 python manage.py migrate
+python manage.py create_guest_user
 python manage.py compilemessages
 
 daphne --bind "0.0.0.0" app.asgi:application
